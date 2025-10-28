aabc01babac08fa89033557b8e109f9ca1cbd0dd
Dérick Campos Santos <164596759+Derickcsantos@users.noreply.github.com>
2025-10-28 10:28:54 -0300

    Remove 'output' property from next.config.ts
    Removed the 'output' property from the Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {}, // garantir que não ative nada experimental automaticamente
-  output: 'standalone', // melhora compatibilidade com SSR na Vercel
+  experimental: {},
   reactStrictMode: true,
+  // ❌ remove a linha abaixo:
+  // output: 'standalone'
 };
 
 export default nextConfig;
